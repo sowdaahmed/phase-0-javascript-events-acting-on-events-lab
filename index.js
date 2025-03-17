d4ed5b01755ac5a822b5ec78b206bacb149aef3c
Sowda <ayanmoha4949@gmail.com>
2025-03-17 04:25:37 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 10}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) { // Assuming the game container is 400px wide
+    dodger.style.left = `${left + 10}px`;
+  }
+}
+
